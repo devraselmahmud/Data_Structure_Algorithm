10c6db66aaa5817fa780a9fc18b14f5a249283ac
Md. Rasel Mahmud <20-1254-h@student.aiub.edu>
2023-10-05 22:05:08 -0700

    Finding Minimum and Maximum Values

--- src/searching/MinMax.java
+++ src/searching/MinMax.java
@@ -1,9 +1,55 @@
 package searching;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Pair{
+	public int min;
+	public int max;
+}
+
 public class MinMax {
 	
+	//sorting and searching min max
+	static Pair getMinMax(int arr[], int n) {
+		
+		Pair minmax = new Pair();
+		Arrays.sort(arr);
+		
+		minmax.min = arr[0];
+		
+		minmax.max = arr[n-1];
+		
+		return minmax;
+		
+	}
+	
 	public static void main(String... args) {
-		System.out.println("Hello World!");
+		
+		//taking user input
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Array Size: ");
+		
+		int n = sc.nextInt();
+		
+		int arr[] = new int[n];
+		
+		System.out.println("Enter an Array: ");
+		
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+			
+		}
+		//closing Scanner
+		sc.close();
+		
+		//calling method
+		Pair minmax = getMinMax(arr, n);
+		
+		System.out.println("Minimum Element is: "+ minmax.min);
+		System.out.println("Maximum Element is: "+ minmax.max);
+		
 	}
 
 }
